9100f695c9001087a0df1b3bbdf24aaf78a979c5
estemobs <estemobsgaming@gmail.com>
2026-04-02 19:48:31 +0200

    Ajout de la fonctionnalité de traduction automatique et intégration de la configuration du mod

---
+++ src/main/java/e/client/Chat_language_translateDataGenerator.java
@@ -0,0 +1,11 @@
+package duke.e.chat_language_translate.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack;
+
+public class Chat_language_translateDataGenerator implements DataGeneratorEntrypoint {
+   public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+      Pack pack = fabricDataGenerator.createPack();
+   }
+}
