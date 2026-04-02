3a0683a0ff9576c02236102efa4e4e01fcb47894
estemobs <estemobsgaming@gmail.com>
2026-04-02 19:57:34 +0200

    Suppression des fichiers de configuration et de services de traduction obsolètes

--- src/main/java/e/Chat_language_translate.java
+++
@@ -1,8 +0,0 @@
-package duke.e.chat_language_translate;
-
-import net.fabricmc.api.ModInitializer;
-
-public class Chat_language_translate implements ModInitializer {
-   public void onInitialize() {
-   }
-}
